357412a2626aa0383978f99d446c8d15b3bc9df7
JesúsÁngelAlcaláPablo <jesusangel.s441233@cesurformacion.com>
2025-10-30 10:23:23 +0100

    Cambio titulo mainframe

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -18,7 +18,7 @@ public class MainFrame extends JFrame {
         this.dataManager = dataManager;
         this.loggedInUser = loggedInUser;
 
-        setTitle("IMDb del chino - Películas de"+loggedInUser.getEmail());
+        setTitle("IMDb made in china - Películas de "+loggedInUser.getEmail());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocationRelativeTo(null); // Centrar la ventana
